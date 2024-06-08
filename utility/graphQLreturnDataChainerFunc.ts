9ce99654bd1b65fcc1bfdd0a55f8f4a96efeb4bc
Frank Collins <fwc3rd@gmail.com>
2024-06-08 11:37:27 -0400

    warmer [11:37am]

--- utility/graphQLreturnDataChainerFunc.ts
+++ utility/graphQLreturnDataChainerFunc.ts
@@ -1,23 +1,17 @@
-import returnMatch from "./regexReturnMatch"
-
 export default async function graphQLreturnDataChainerFunc(query:string, data:any) {
     // data.data.${functionName} this function chains data by comparing, [i]->[i] Object.Keys() & Object.Values()
 // might not be used right away until app-wide known to not fail but might use as:              data = data?.data?.endpoint || data (correct everytime?
 
     // thx chatGPT saving incalculable numbers of hours determining this solution:
     const pattern = /\{\s*([^\{\(\s]+)\(/;
-    const matchResult = await returnMatch(pattern, query)
+    // const matchResult = await returnMatch(pattern, query)
     // const string = "data.data.matchResult"
     
     let prechain = data?.data?.data
     let objectKeys = Object.keys(prechain)
     let objectValues = Object.values(prechain)
 
-    if (objectKeys[0] === matchResult) {
-        return objectValues;
-    } else {
-        return null;
-    }        
+    return objectValues;
 }
 
 // that algo grabs   {doWeBlockEachOther|graphQLresolverName} regex that grabs b/w first instance of: "{"     "("     opening bracket & parenthesis
