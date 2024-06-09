e2e782f6069c5d046f572f5d8598820ecf9b007d
Frank Collins <fwc3rd@gmail.com>
2024-06-09 08:42:21 -0400

    denormalize data with new field for estimates interface [8:42am]

--- utility/analystData.ts
+++ utility/analystData.ts
@@ -4,50 +4,51 @@ const bofaEstimate:estimateINTERFACE = {
     estimate: 285,
     rationale: "BofA saw increased investor confidence due to positive earnings reports.",
 // analystData: { relativity: { current }} 10:00 hour b4 deploy link relativity.current to calendar & can look at different days.
-    relativity: { current: 404, variance: null, company: 'bofa' }
+    relativity: { current: 404, variance: null, company: 'bofa', ticker: "bac" }
     // date: "6-1-2024"
 }
 
 const citibankEstimate:estimateINTERFACE = {
     estimate: 365,
     rationale: "Citibank's stability was attributed to steady performance in its core banking operations.",
-    relativity: { current: 404, variance: null, company: 'citibank' }
+// relativity could also have {primary_exchange} maybe that's an uninformed take but that info is listed on google when looking up tickers.
+    relativity: { current: 404, variance: null, company: 'citibank', ticker: 'c' }
 }
 
 const goldmanSachsEstimate:estimateINTERFACE = {
     estimate: 322,
     rationale: "Goldman Sachs benefited from strong performance in its investment banking division.",
-    relativity: { current: 404, variance: null, company: 'goldmanSachs' }
+    relativity: { current: 404, variance: null, company: 'goldmanSachs', ticker: 'gs' }
 }
 
 const morganStanleyEstimate:estimateINTERFACE = {
     estimate: 440,
     rationale: "Morgan Stanley's growth was driven by favorable market conditions and strategic acquisitions.",
-    relativity: { current: 404, variance: null, company: 'morganStanley' }
+    relativity: { current: 404, variance: null, company: 'morganStanley', ticker: 'ms' }
 }
 
 const jpMorganEstimate:estimateINTERFACE = {
     estimate: 405,
     rationale: "Morgan Stanley's growth was driven by favorable market conditions and strategic acquisitions.",
-    relativity: { current: 404, variance: null, company: 'jpMorgan' }
+    relativity: { current: 404, variance: null, company: 'jpMorgan', ticker: 'jpm' }
 }
 
 const moelisEstimate:estimateINTERFACE = {
     estimate: 275,
     rationale: "Moelis experienced a moderate increase following successful advisory deals.",
-    relativity: { current: 404, variance: null, company: 'moelis' }
+    relativity: { current: 404, variance: null, company: 'moelis', ticker: 'mc' }
 }
 
 const lazardEstimate:estimateINTERFACE = {
     estimate: 295,
     rationale: "Lazard's gradual rise was due to strong performance in asset management and financial advisory services.",
-    relativity: { current: 404, variance: null, company: 'lazard' }
+    relativity: { current: 404, variance: null, company: 'lazard', ticker:  'laz' }
 }
 
 const evercoreEstimate:estimateINTERFACE = {
     estimate: 380,
     rationale: "Evercore's steady upward trend was supported by consistent revenue growth and successful deal closures.",
-    relativity: { current: 404, variance: null, company: 'evercore' }
+    relativity: { current: 404, variance: null, company: 'evercore', ticker: 'evr' }
 }
 
     // AItentativeESTIMATE is a wishlist-item: might have it's own interface.
