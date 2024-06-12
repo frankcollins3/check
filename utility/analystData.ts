ff212d9802a32aae8a16ccd4dea8b2fea230d48d
Frank Collins <fwc3rd@gmail.com>
2024-06-12 00:17:17 -0400

    analystData update with ticker string [12:17am]

--- utility/analystData.ts
+++ utility/analystData.ts
@@ -5,7 +5,7 @@ const bofaEstimate:estimateINTERFACE = {
     rationale: "Increased investor confidence stemmed from positive earnings reports related to Apple. This could imply strong quarterly results, driven by robust iPhone sales and successful expansion into new markets like wearables and services.",
 // analystData: { relativity: { current }} 10:00 hour b4 deploy link relativity.current to calendar & can look at different days.
 // relativity: { current: 404, variance: null, company: 'bofa', ticker: "bac" }
-    relativity: { current: 404, variance: null, company: 'Bank of America' }
+    relativity: { current: 404, variance: null, company: 'Bank of America', ticker: 'bofa', }
     // date: "6-1-2024"
 }
 
@@ -13,43 +13,43 @@ const citibankEstimate:estimateINTERFACE = {
     estimate: 365,
     rationale: "Stability in Apple's core business operations led to a positive outlook. This may suggest consistent revenue streams from various product lines, including iPhones, Macs, and services like iCloud and Apple Music.",
 // relativity could also have {primary_exchange} maybe that's an uninformed take but that info is listed on google when looking up tickers.
-    relativity: { current: 404, variance: null, company: 'Citibank' }
+    relativity: { current: 404, variance: null, company: 'Citibank', ticker: 'c' }
 }
 
 const goldmanSachsEstimate:estimateINTERFACE = {
     estimate: 322,
     rationale: "Strong performance in Apple's investment and innovation initiatives contributed to a favorable rating. This might reflect successful product launches, strategic investments, and advancements in services like Apple TV+ and Apple Arcade.",
-    relativity: { current: 404, variance: null, company: 'Goldman Sachs' }
+    relativity: { current: 404, variance: null, company: 'Goldman Sachs', ticker: 'gs' }
 }
 
 const morganStanleyEstimate:estimateINTERFACE = {
     estimate: 440,
     rationale: "Favorable market conditions and strategic decisions propelled Apple's growth outlook. This could include optimism surrounding upcoming product releases, potential expansion into new markets, and continued dominance in existing ones.",
-    relativity: { current: 404, variance: null, company: 'Morgan Stanley' }
+    relativity: { current: 404, variance: null, company: 'Morgan Stanley', ticker: 'ms' }
 }
 
 const jpMorganEstimate:estimateINTERFACE = {
     estimate: 405,
     rationale: "Positive market conditions and strategic initiatives drove JP Morgan's confidence in Apple. This might be attributed to strong demand for Apple products, anticipated revenue growth, and effective capital management strategies.",
-    relativity: { current: 404, variance: null, company: 'JP Morgan' }
+    relativity: { current: 404, variance: null, company: 'JP Morgan', ticker: 'jpm' }
 }
 
 const moelisEstimate:estimateINTERFACE = {
     estimate: 275,
     rationale: "Apple saw a moderate increase following successful business ventures. This could signify steady growth fueled by product innovation, customer loyalty, and potential market expansion.",
-    relativity: { current: 404, variance: null, company: 'Moelis' }
+    relativity: { current: 404, variance: null, company: 'Moelis', ticker: 'mc' }
 }
 
 const lazardEstimate:estimateINTERFACE = {
     estimate: 295,
     rationale: "Gradual rise in Apple's stock value due to strong performance in key business areas. This may include effective leadership, innovation, and market positioning in comparison to competitors.",
-    relativity: { current: 404, variance: null, company: 'Lazard' }
+    relativity: { current: 404, variance: null, company: 'Lazard', ticker: 'laz' }
 }
 
 const evercoreEstimate:estimateINTERFACE = {
     estimate: 380,
     rationale: "Consistent revenue growth and successful business deals supported Evercore's positive assessment of Apple. This could indicate strong financial performance, promising future prospects, and effective execution of corporate strategies.",
-    relativity: { current: 404, variance: null, company: 'Evercore' }
+    relativity: { current: 404, variance: null, company: 'Evercore', ticker: 'evr' }
 }
 
     // AItentativeESTIMATE is a wishlist-item: might have it's own interface.
